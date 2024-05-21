5776b0aadebda2b73a309aac5f7288400becbed0
sebastian123gonzalo <sebastian123gonzalo@gmail.com>
2024-05-21 15:32:48 -0500

    Fix: store.js fixed

--- src/store/store.js
+++ src/store/store.js
@@ -5,6 +5,9 @@ export default createStore({
         user_id: localStorage.getItem('user_id') || null,
         user_type: localStorage.getItem('user_type') || null,
         isActive: false,
+        theme: localStorage.getItem('theme') || 'light',
+        view: localStorage.getItem('view') || 'grid',
+        initialConfig: null,
     },
     mutations: {
 
@@ -19,6 +22,37 @@ export default createStore({
         setIsActive(state, value) { // Nueva mutación para isActive
             state.isActive = value;
             localStorage.setItem('isActive', value);
-        }
+        },
+        SET_THEME(state, theme) {
+            state.theme = theme;
+            localStorage.setItem('theme', theme);
+            this.commit('APPLY_THEME'); // Llama a la nueva mutación
+        },
+        APPLY_THEME(state) { // Nueva mutación para aplicar el tema
+            document.body.className = state.theme;
+        },
+        SET_VIEW(state, view) { // nueva mutación
+            state.view = view;
+            localStorage.setItem('view', view);
+        },
+        SET_INITIAL_CONFIG(state) { // nueva mutación para establecer la configuración inicial
+            state.initialConfig = {
+                theme: state.theme,
+                view: state.view,
+            };
+        },
+        RESET_CONFIG(state) { // nueva mutación para restablecer la configuración
+            if (state.initialConfig) {
+                state.theme = state.initialConfig.theme;
+                state.view = state.initialConfig.view;
+                localStorage.setItem('theme', state.theme);
+                localStorage.setItem('view', state.view);
+                this.commit('APPLY_THEME'); // Llama a la nueva mutación
+            }
+        },
+    },
+    getters: {
+        getTheme: state => state.theme,
+        getView: state => state.view
     }
 });
